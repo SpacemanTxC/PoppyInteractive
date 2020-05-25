77a0710471d9faee257b0371749f6cf1f5effe83
Tate Capra <tatecapra@outlook.com>
2020-05-25 16:05:12 -0500

    fixing bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,9 @@ let newPhoto =
   ["../photo/poppy1.jpg", "../photo/poppy2.jpg" , "../photo/poppy3.jpg"];
 
 //var to store the new html that will replace the index page
-let photo1Layout = `<img src ="assets/photo/poppy1.jpg" class ="img-fluid ">`;
-let photo2Layout = `<img src ="assets/photo/poppy2.jpg" class ="img-fluid ">`;
-let photo3Layout = `<img src ="assets/photo/poppy3.jpg" class ="img-fluid ">`;
+let photo1Layout = `<img src ="assets/photo/poppy1.jpg" class ="img-fluid">`;
+let photo2Layout = `<img src ="assets/photo/poppy2.jpg" class ="img-fluid">`;
+let photo3Layout = `<img src ="assets/photo/poppy4.jpg" class ="img-fluid">`;
 
 // Function to change image
 function changePhoto(){
@@ -25,6 +25,7 @@ function changePhoto(){
       //Else statement to end if loop, prints new img, and resets check value
     }else if(numCheck == 2){
       document.getElementById('poppy1').innerHTML = photo1Layout;
+      console.log("Test image 3");
       numCheck++;
     }else {
       numCheck = 0;
